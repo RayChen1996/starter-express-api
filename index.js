170d6f77960814e1d4dc6bd659ede95ef6358911
RayChen1996 <53119155+RayChen1996@users.noreply.github.com>
2023-12-02 20:07:09 +0800

    update port

--- index.js
+++ index.js
@@ -89,11 +89,11 @@ async function startServer() {
   const server = new ApolloServer({ typeDefs, resolvers });
   await server.start();
   server.applyMiddleware({ app });
-
-  const PORT = process.env.PORT || 4000;
-  app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}${server.graphqlPath}`);
-  });
+  app.listen(process.env.PORT || 3000)
+//   const PORT = process.env.PORT || 4000;
+//   app.listen(PORT, () => {
+//     console.log(`Server running on http://localhost:${PORT}${server.graphqlPath}`);
+//   });
 }
 
 startServer();
