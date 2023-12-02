4c14dd9e18c5a5d4103e1b0b10cefb61b8f8d728
RayChen1996 <53119155+RayChen1996@users.noreply.github.com>
2023-12-02 22:16:50 +0800

    更新 schema  habit

--- index.js
+++ index.js
@@ -1,33 +1,64 @@
-// const express = require('express')
-// const app = express()
-// app.all('/', (req, res) => {
-//     console.log("Just got a request!")
-//     res.send('Yo!')
-// })
-// app.listen(process.env.PORT || 3000)
+const { ApolloServer, gql } = require("apollo-server");
+ 
+const mongoose = require("mongoose");
 
-const express = require('express');
-const { ApolloServer, gql } = require('apollo-server-express');
-const mongoose = require('mongoose');
+
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+// const typeDefs = require("./schema/type");
+// const mongoose = require("mongoose");
+require("dotenv").config();
+const uri =
+  "mongodb+srv://ray10315332:GayqbQeJq5Jxh3em@cluster0.pberq7k.mongodb.net/";
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-async function startServer() {
-  const app = express();
+const connection = mongoose.connection;
 
-  // Replace 'your_connection_string' with your actual MongoDB connection string
-  await mongoose.connect('mongodb+srv://ray10315332:GayqbQeJq5Jxh3em@cluster0.pberq7k.mongodb.net', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+connection.once("open", () => {
+  console.log("mogoose connect!!!");
+});
+const User = mongoose.model('User', {
+  email: String,
+  password: String,
+});
+
+
+const Habitgoals = mongoose.model('habitgoals', {
+    Goal: String,
+    type: Number,
+    frequency:String,
+    fine: Number,
+    note:String,
+    SDate:String,
+    EDate:String
   });
+// 定义GraphQL Schema
+const typeDefs = gql`
+  type Todo {
+    id: ID!
+    task: String!
+    completed: Boolean!
+  }
 
-  const User = mongoose.model('User', {
-    email: String,
-    password: String,
-  });
 
-  const typeDefs = gql`
-    type User {
+
+  type Habits {
+    id: ID!
+    Goal: String!
+    type: Int!
+    frequency:String!
+    fine: Int!
+    note:String!
+    SDate:String!
+    EDate:String
+  }
+ 
+
+ 
+  type User {
       id: ID!
       email: String!
     }
@@ -37,63 +68,143 @@ async function startServer() {
     }
 
     type Query {
-      users: [User]
+        users: [User]
+        todos: [Todo]
+        todo(id: ID!): Todo
+        Habits:[Habits]
     }
 
     type Mutation {
-      register(email: String!, password: String!): AuthPayload
-      login(email: String!, password: String!): AuthPayload
+        register(email: String!, password: String!): AuthPayload
+        login(email: String!, password: String!): AuthPayload
+        addTodo(task: String!): Todo
+        updateTodo(id: ID!, task: String!, completed: Boolean!): Todo
+        deleteTodo(id: ID!): Todo
+        addHabitGoal(Goal: String!): Habits
+        deleteHabits: [Habits]
+
     }
-  `;
 
-  const resolvers = {
-    Query: {
-      users: () => User.find(),
+
+
+`;
+
+// 4. 提供解析函数
+const resolvers = {
+  Query: {
+    users: () => {
+      try {
+        console.log('Executing users resolver');
+        return User.find();
+      } catch (error) {
+        console.error('Error in users resolver:', error);
+        throw error; // Re-throw the error to ensure it's propagated
+      }
     },
-    Mutation: {
-      register: async (_, { email, password }) => {
-        const existingUser = await User.findOne({ email });
-
-        if (existingUser) {
-          throw new Error('User already exists');
-        }
-
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const user = new User({ email, password: hashedPassword });
-        await user.save();
-
-        const token = jwt.sign({ userId: user.id }, 'your_secret_key', { expiresIn: '7d' });
-
-        return { token };
-      },
-      login: async (_, { email, password }) => {
-        const user = await User.findOne({ email });
-
-        if (!user) {
-          throw new Error('Invalid login credentials');
-        }
-
-        const passwordMatch = await bcrypt.compare(password, user.password);
-
-        if (!passwordMatch) {
-          throw new Error('Invalid login credentials');
-        }
-
-        const token = jwt.sign({ userId: user.id }, 'your_secret_key', { expiresIn: '7d' });
-
-        return { token };
-      },
+    todos: async () => {
+      const todos = await Todo.find();
+      return todos;
     },
-  };
+    todo: async (_, { id }) => {
+      const todo = await Todo.findById(id);
+      return todo;
+    },
+    Habits: async () => {
+        const habits = await Habitgoals.find();
+        return habits;
+    },
+  },
+  Mutation: {
+    register: async (_, { email, password }) => {
+      const existingUser = await User.findOne({ email });
 
-  const server = new ApolloServer({ typeDefs, resolvers });
-  await server.start();
-  server.applyMiddleware({ app });
-  app.listen(process.env.PORT || 3000)
-//   const PORT = process.env.PORT || 4000;
-//   app.listen(PORT, () => {
-//     console.log(`Server running on http://localhost:${PORT}${server.graphqlPath}`);
-//   });
-}
+      if (existingUser) {
+        throw new Error('User already exists');
+      }
 
-startServer();
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const user = new User({ email, password: hashedPassword });
+      await user.save();
+
+      const token = jwt.sign({ userId: user.id }, 'your_secret_key', { expiresIn: '7d' });
+
+      return { token };
+    },
+    login: async (_, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new Error('Invalid login credentials');
+      }
+
+      const passwordMatch = await bcrypt.compare(password, user.password);
+
+      if (!passwordMatch) {
+        throw new Error('Invalid login credentials');
+      }
+
+      const token = jwt.sign({ userId: user.id }, 'your_secret_key', { expiresIn: '7d' });
+
+      return { token };
+    },
+    addTodo: async (_, { task }) => {
+      const todo = new Todo({ task, completed: false });
+      await todo.save();
+      return todo;
+    },
+    updateTodo: async (_, { id, task, completed }) => {
+      const todo = await Todo.findByIdAndUpdate(
+        id,
+        { task, completed },
+        { new: true }
+      );
+      return todo;
+    },
+    deleteTodo: async (_, { id }) => {
+      const todo = await Todo.findByIdAndDelete(id);
+      return todo;
+    },
+    deleteHabits: async () => {
+        try {
+          const deletedHabits = await Habitgoals.deleteMany({});
+          return deletedHabits;
+        } catch (error) {
+          console.error('Error deleting habits:', error);
+          throw error;
+        }
+      },
+    addHabitGoal: async (_, { Goal }) => {
+        const habit = new Habitgoals({  Goal  });
+        await habit.save();
+        return habit;
+    },
+  },
+};
+
+// 5. 连接到 MongoDB 数据库
+mongoose.connect(
+  "mongodb+srv://ray10315332:GayqbQeJq5Jxh3em@cluster0.pberq7k.mongodb.net/",
+  {
+    useUnifiedTopology: true,
+  }
+);
+
+// 6. 定义 Todo 模型
+const Todo = mongoose.model("Todo", {
+  task: String,
+  completed: Boolean,
+});
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  
+  persistedQueries: false,
+  playground: true,
+});
+
+ 
+
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+  console.log(`Server is ${url}`);
+});
